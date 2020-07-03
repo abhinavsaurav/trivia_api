eb02bd9be623813f21053ed85f927423a2f1f053
abhinav <abhinavsaurav1@gmail.com>
2020-07-04 03:20:14 +0530

    [bugged] /quizzes route working but render issue

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -155,23 +155,21 @@ def create_app(test_config=None):
   '''
   @app.route('/questionsSearch',methods=['POST'])
   def question_by_phrase():
-    print("line1")
+    # print("line1")
     data=request.get_json()['searchTerm']
-    print("printing this",data)
+    # print("printing this",data)
     
     questions =Question.query.filter(Question.question.ilike('%{}%'.format(data))).all()
-    print("reaching here")
+    # print("reaching here")
     formatted_quest= [ question.format() for question in questions]
     
     
     return jsonify({
       "success":True,
       "questions":formatted_quest,
-      "totalQuestions":len(questions),
-      "currentCategory":None
+      "total_questions":len(questions),
+      "current_category":None
     })
-    # except:
-    #   abort(422)
 
   '''
   @TODO: 
@@ -181,7 +179,21 @@ def create_app(test_config=None):
   categories in the left column will cause only questions of that 
   category to be shown. 
   '''
-
+  @app.route('/categories/<int:catg_id>/questions',methods=['GET'])
+  def questionByCategory(catg_id):
+    try:
+      # questions=db.session.query(Category,Question).join(Question,Question.category==Category.id).filter(Category.id==catg_id).all()
+      # The above query will return from both the tables where as below query will join but return only from questions 
+      questions=Question.query.join(Category, Question.category==Category.id).filter(Category.id==catg_id).all()
+      formatted_quest=[question.format() for question in questions]
+      return jsonify({
+        "success":True,
+        "questions":formatted_quest,
+        "total_questions":len(questions),
+        "current_category":None
+      })
+    except :
+      abort(404)
 
   '''
   @TODO: 
@@ -194,7 +206,81 @@ def create_app(test_config=None):
   one question at a time is displayed, the user is allowed to answer
   and shown whether they were correct or not. 
   '''
-
+  @app.route('/quizzes',methods=['POST'])
+  def quiz():
+    try:
+      data=request.get_json()
+      # print(data[''])
+      question_ids=data['previous_questions']
+      quiz_catg=data['quiz_category']
+      print(question_ids,"--",quiz_catg)
+      if quiz_catg['id'] == 0:
+        questions=Question.query.all()
+      else:
+        questions=Question.query.join(Category,Category.id==Question.category).filter(~Question.id.in_(question_ids),Category.id == quiz_catg['id']).all()
+      # if not quiz_catg:
+      #   abort(422)
+      
+      print(questions)
+      returned_ids=[]
+      randomQuestionData=[]
+      found=False
+      if questions:
+        returned_ids=[question.id for question in questions]
+        print(returned_ids)
+      if returned_ids:
+        randomQuestionId=random.choice(returned_ids)
+        for question in questions:
+          if question.id==randomQuestionId:
+            randomQuestionData.append(question.format())
+            found=True
+            break
+      
+      if not found:
+        return jsonify({
+          "success":True,
+          "questions":None
+        }) ,404
+      else:
+        return jsonify({
+            "success":True,
+            "previous_questions":data['previous_questions'],
+            "question": randomQuestionData
+        })
+    except :
+      abort(422)
+    
+    #   try:
+  #     data = request.get_json()
+  #     # check given category
+  #     category_id = int(data["quiz_category"])
+  #     category = Category.query.get(category_id)
+  #     previous_questions = data["previous_questions"]
+  #     if not category == None:  
+  #       if "previous_questions" in data and len(previous_questions) > 0:
+  #         questions = Question.query.filter(
+  #           Question.id.notin_(previous_questions),
+  #           Question.category == category.id
+  #           ).all()  
+  #       else:
+  #         questions = Question.query.filter(Question.category == category.id).all()
+  #     else:
+  #       if "previous_questions" in data and len(previous_questions) > 0:
+  #         questions = Question.query.filter(Question.id.notin_(previous_questions)).all()  
+  #       else:
+  #         questions = Question.query.all()
+  #     max = len(questions) - 1
+  #     if max > 0:
+  #       question = questions[random.randint(0, max)].format()
+  #     else:
+  #       question = False
+  #     return jsonify({
+  #       "success": True,
+  #       "question": question
+  #     })
+  #   except:
+  #     abort(500, "An error occured while trying to load the next question")
+    
   '''
   @TODO: 
   Create error handlers for all expected errors 
